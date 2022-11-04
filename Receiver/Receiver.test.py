55bc8ef444f4ca55ff61c583b2578aefaf0c5da1
sriharsha annamaneni <sriharsha0806@gmail.com>
2022-11-04 12:04:57 +0530

    Update Receiver.test.py

--- Receiver/Receiver.test.py
+++ Receiver/Receiver.test.py
@@ -2,6 +2,7 @@ import unittest
 from Receiver import * 
 
 class receiver_test(unittest.TestCase):
+    # testing
     def test_range_and_count(self):
         self.assertEqual(statistics.calc_min([111914360, 678148338, 624407569, 1451916718, 147884402, 1875150203, 1896242708]), 111914360)
         self.assertEqual(statistics.calc_min([0, 8, 9, 8, 2, 3, 8]), 0)
